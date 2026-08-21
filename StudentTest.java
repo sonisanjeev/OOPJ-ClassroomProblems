eee080a345365c401a6c8703edbcd19478dac5be
sonisanjeev <sonisanjeev1990@gmail.com>
2026-08-21 11:50:45 +0530

    Problem 1 Done

---
+++ StudentTest.java
@@ -0,0 +1,43 @@
+class Student{
+   private String name;
+    private int rollno;
+    private double marks;
+    static int studentCount=0;
+
+    Student(String name, int rollno, double makrs){
+        this.name=name;
+        this.rollno=rollno;
+        this.marks=makrs;
+        studentCount++;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setRollno(int rollno){
+        this.rollno=rollno;
+    }
+    public int getRollno(){
+        return rollno;
+    }
+
+    public void setMarks(double marks){
+        this.marks=marks;
+    }
+    public double getMarks(){
+        return marks;
+    }
+    
+}
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student("Rahul", 101, 85.5);
+        Student s2 = new Student("Priya", 102, 91.0);
+        System.out.println(s1.getName());
+        System.out.println("Marks: " + s2.getMarks());
+        System.out.println("Total Students: " + Student.studentCount);
+    }
+}
